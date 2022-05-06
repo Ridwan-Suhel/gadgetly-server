0744c09d7a43d22be0fa6d4256f4f6b620b84678
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-06 23:19:12 +0600

    json test data added

--- index.js
+++ index.js
@@ -14,17 +14,35 @@ app.get("/", (req, res) => {
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jv7le.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("db connected test msg");
-  client.close();
-});
+// client.connect((err) => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log("db connected test msgs");
+//   client.close();
+// });
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("gadgetlydb").collection("products");
+    console.log("db connected test msgs");
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("Server running on port-", port);
