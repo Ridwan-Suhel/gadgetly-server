6b6889d9cb0d22a1790ebdf4bddf9d696b29220d
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-06 22:11:24 +0600

    db connected

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello from gadgetly");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jv7le.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("db connected");
+  client.close();
 });
 
 app.listen(port, () => {
