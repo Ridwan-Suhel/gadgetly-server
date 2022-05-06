235b56d80ba141335851664b866b3ffd8edf5cd9
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-07 00:41:28 +0600

    single product api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,12 @@ async function run() {
       const cursor = productCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
+    });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productCollection.findOne(query);
+      res.send(product);
     });
   } finally {
     // await client.close();
