4213f167661801a40c49789dd2e67d9fbf2d50df
Ridwan-Suhel <ridwansuhel96@gmail.com>
2022-05-08 00:42:50 +0600

    post api added

--- index.js
+++ index.js
@@ -70,6 +70,12 @@ async function run() {
       const result = await productCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.post("/product", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    });
   } finally {
     // await client.close();
   }
